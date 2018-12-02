3b523d695f135fcf4899db294d364a50a4efe75a
falcon <aron.fiechter@gmail.com>
2018-12-02 17:53:18 +0100

    Move function to separate file

--- src/js/sqrfun.js
+++ src/js/sqrfun.js
@@ -173,26 +173,6 @@ function computeDelay(nodes, startNode, endNode) {
   return (endDate - startDate);
 }
 
-function totalNumberOfRequests(nodes) {
-  var counter = 0;
-  var totalNumberOfRequests = {};
-  var max = Number.MIN_VALUE;
-  for (var key in nodes) {
-    for (var status in nodes[key]) {
-      counter += (nodes[key][status].statusArray.length);
-    }
-    totalNumberOfRequests[key] = counter;
-    if (max < counter) {
-      max = counter;
-    }
-    counter = 0;
-  }
-  return {
-    "total": totalNumberOfRequests,
-    "maxRequests": max
-  };
-}
-
 function getSelectedIPs(clients, elem) {
   var result = {};
   var options = elem && elem.options;
