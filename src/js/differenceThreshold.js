a19f29224cdaefdfc68bc06ece2632113067b9ed
falcon <aron.fiechter@gmail.com>
2018-12-03 22:17:26 +0100

    Refactor differenceThreshold into smaller functions

--- src/js/differenceThreshold.js
+++ src/js/differenceThreshold.js
@@ -1,45 +1,78 @@
-function differenceThreshold(client) {
-  client = sortLogs(client);
-  let avg = 0;
-  let min = Number.MAX_VALUE;
-  let max = Number.MIN_VALUE;
-  let timePeriods = [];
-  for (let i = 1; i < client.length; i++) {
-    let date = new Date(client[i - 1].datetime);
-    let date1 = new Date(client[i].datetime);
-    let diff = Math.abs(date1 - date);
-    avg += diff;
-    if (max < diff) max = diff;
-    if (min > diff) min = diff;
+/**
+ * Given a list of clients, cluster them into smaller lists representing
+ * close time intervals.
+ *
+ * @param {Array<Object>} clientLogs list of clients
+ * @returns {Array<Array<Object>>} client logs clustered into lists of "close" time intervals
+ */
+function differenceThreshold(clientLogs) {
+  const sortedClientLogs = sortClientsByLogTime(clientLogs);
+
+  // Get intervals and compute interval length threshold
+  const intervals = computeIntervals(sortedClientLogs);
+  const min = Math.min(...intervals);
+  const max = Math.max(...intervals);
+  const threshold = (min + max) / 2;
+
+  // Aggregate clients into time periods (by closeness)
+  return splitIntoClusters(clientLogs, intervals, threshold);
+}
+
+/**
+ * Sort client logs by datetime.
+ *
+ * @param {Array<Object>} clientLogs list of clients
+ * @returns {Array<Object>} sorted clientLogs
+ */
+function sortClientsByLogTime(clientLogs) {
+  clientLogs.sort((a, b) => new Date(a.datetime) - new Date(b.datetime));
+  return clientLogs;
+}
+
+/**
+ * Compute intervals of time between client logs.
+ *
+ * @param {Array<Object>} clientLogs list of clients
+ * @returns {Array<Number>} list of length of time intervals between client logs
+ */
+function computeIntervals(clientLogs) {
+  const intervals = [];
+  for (let i = 1; i < clientLogs.length; i++) {
+    const d1 = new Date(clientLogs[i - 1].datetime);
+    const d2 = new Date(clientLogs[i].datetime);
+    intervals.push(Math.abs(d2 - d1));
   }
-  avg /= (client.length - 1);
-  let diffThreshold = (min + max) / 2;
-  let timeP = [];
-  timeP.push(client[0]);
-  for (let i = 1; i < client.length; i++) {
-    let date = new Date(client[i - 1].datetime);
-    let date1 = new Date(client[i].datetime);
-    let diff = Math.abs(date1 - date);
-    if (diff > diffThreshold) {
-      if (timeP == []) timeP.push(client[i - 1]);
-      timePeriods.push(timeP);
-      timeP = [];
+  return intervals;
+}
+
+/**
+ *
+ * @param {Array<Object>} clientLogs list of clients
+ * @param {Array<Number>} intervals list of length of time intervals between client logs
+ * @param {Number} tol tolerance value
+ * @returns {Array<Array<Object>>} client logs clustered into lists of "close" time intervals
+ */
+function splitIntoClusters(clientLogs, intervals, tol) {
+  const clientClusters = [];
+
+  // First cluster contains first client by default
+  let currCluster = [clientLogs[0]];
+
+  for (let i = 1; i < clientLogs.length; i++) {
+    // Get interval between current and previous client
+    const interval = intervals[i - 1];
+
+    // If interval is longer than tolerance, close current interval
+    if (interval > tol) {
+      clientClusters.push(currCluster);
+      currCluster = [];
     }
-    timeP.push(client[i]);
+
+    // Add client to current cluster (or to next cluster if it was cleared)
+    currCluster.push(clientLogs[i]);
   }
-  if (timeP != []) timePeriods.push(timeP);
-  return timePeriods;
-}
 
-function sortLogs(clients) {
-  clients.sort((a, b) => compare(a.datetime, b.datetime));
-  return clients;
-}
-
-function compare(a, b) {
-  a = new Date(a);
-  b = new Date(b);
-  if (a > b) return 1;
-  else if (a === b) return 0;
-  else return -1;
+  // Add last cluster if not trivial and return clusters
+  if (currCluster.length !== 0) clientClusters.push(currCluster);
+  return clientClusters;
 }
